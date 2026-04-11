895eb4aea4745c6e3b486cbfd32c5c6810032545
Jorge Sáez <jsaezvillate@gmail.com>
2026-04-11 14:12:27 +0200

    Actualizacion Mejora tema 1

--- src/utils/generador_escenarios.py
+++ src/utils/generador_escenarios.py
@@ -6,6 +6,13 @@ def generador_recursivo_escenarios(n, id_actual):
         return []
     #El caso general, creamos un pedido con un peso y beneficio aleatorio. 
     pedido_nuevo = Pedido (id_actual, random.randint(4,10), random.randint(20,70))
+    datos_pedido = {
+        "id": pedido_nuevo.id,
+        "peso": pedido_nuevo.peso,
+        "beneficio": pedido_nuevo.beneficio
+    }
     #La relacion de recurrencia, cuando tenemos pedidos los vamos incluyendo todos de 
     #forma consecutiva concatenandolos y pasandolos a diccionario para poder usarlo mejor en los archivos de escenario.
-    return [pedido_nuevo.to_dict()] + generador_recursivo_escenarios(n-1, id_actual+1)
+    return [datos_pedido] + generador_recursivo_escenarios(n-1, id_actual+1)
+if __name__ == "__main__":
+    print(generador_recursivo_escenarios(5, 1))
